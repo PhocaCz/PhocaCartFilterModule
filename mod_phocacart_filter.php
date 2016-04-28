2e64f3d5bcb68f0edb1ba57ff1276619ca079e8a
Jan <info@phoca.cz>
2016-04-28 23:57:50 +0200

    3.0.0RC2

--- mod_phocacart_filter.php
+++ mod_phocacart_filter.php
@@ -10,7 +10,9 @@
 defined('_JEXEC') or die('Restricted access');// no direct access
 
 if (!JComponentHelper::isEnabled('com_phocacart', true)) {
-	return JError::raiseError(JText::_('Phoca Cart Error'), JText::_('Phoca Cart is not installed on your system'));
+	$app = JFactory::getApplication();
+	$app->enqueueMessage(JText::_('Phoca Cart Error'), JText::_('Phoca Cart is not installed on your system'), 'error');
+	return;
 }
 if (! class_exists('PhocaCartLoader')) {
     require_once( JPATH_ADMINISTRATOR.'/components/com_phocacart/libraries/loader.php');
@@ -26,9 +28,7 @@ $lang->load('com_phocacart');
 $document					= JFactory::getDocument();
 $document->addScript(JURI::root(true).'/media/com_phocacart/js/filter/jquery.ba-bbq.min.js');
 $document->addScript(JURI::root(true).'/media/com_phocacart/js/filter/filter.js');
-$isItemsView 				= PhocaCartRoute::isItemsView();
-$urlItemsView 				= PhocaCartRoute::getJsItemsRoute();
-$urlItemsViewWithoutParams 	= PhocaCartRoute::getJsItemsRouteWithoutParams();
+
 
 $filter						= new PhocaCartFilter();
 $filter->category			= $params->get( 'filter_category', 0 );
@@ -37,7 +37,25 @@ $filter->manufacturer 		= $params->get( 'filter_manufacturer', 1 );
 $filter->price 				= $params->get( 'filter_price', 1 );
 $filter->attributes 		= $params->get( 'filter_attributes', 0 );
 $filter->specifications 	= $params->get( 'filter_specifications', 0 );
-$p['remove_parameters_cat']	= $params->get( 'filter_specifications', 0 );
+$p['remove_parameters_cat']	= $params->get( 'remove_parameters_cat', 0 );
+
+$app	= JFactory::getApplication();
+$isItemsView 				= PhocaCartRoute::isItemsView();
+
+$urlItemsView 				= PhocaCartRoute::getJsItemsRoute($filter->category);
+$urlItemsViewWithoutParams 	= PhocaCartRoute::getJsItemsRouteWithoutParams();
+$config 					= JFactory::getConfig();
+$sef						= $config->get('sef', 1);
+
+
+/* Difference between - Active category vs. All categories
+ * Active category - Url gets ID parameter and it can be only one ID: id=1:abc
+ * All categories - Url gets c parameter and there can be more categories filters: c=1:abc,2def
+ * When we use all categories, we need clean urlItemsView
+ */
+if ($filter->category == 2) {
+	$urlItemsView = $urlItemsViewWithoutParams;
+}
 
 /*
  * param 		... param name, for example tag
@@ -55,21 +73,35 @@ $p['remove_parameters_cat']	= $params->get( 'filter_specifications', 0 );
  *				    parameter (price_to), so we don't reload the site but we build the url with hel of global variable
  */
  
-
-// Specific case for deselecting categories
+//krumo($p['remove_parameters_cat']);
+// Specific case for deselecting categories (ACTIVE CATEGORY ONLY)
 if ($p['remove_parameters_cat'] == 1) {
 	// If set in parameters to YES, when deselecting category, all other params will be removed too
 	// RECOMMENDED as mostly the parameters can be assigned to category
-	$jsPart1 = 'document.location 		= urlItemsView';
+	$jsPart1 = 'document.location 		= urlItemsView;';
 } else {
 	// If set in parameters to NO, when deleting category, all other parameters will stay in URL
 	// NOT RECOMMENDED as mostly all other parameters are assigned to category
+	// If we are not in items views (isItemsView = 0), there cannot be even any parameters,
+	// so we can reload to pure clean items view (urlItemsView) when we remove the category
+	//
+	// Remove category parameter from GET
+	// IF NO ITEMS VIEW - we can remove everything and go back to items view (e.g. we are in category view)
+	// IF SEF DISABLED - category id: id=5 is standard parameter and we need to remove it with help of phRemoveFilter
+	// IF SEF ENABLED - category id: 5-category is not standard parameter but alias, remove it with querystring
 	$jsPart1 = 'var currentUrlParams	= jQuery.param.querystring();'
-			  .'document.location 		= jQuery.param.querystring(urlItemsView, currentUrlParams, 2);';
+		.' if (isItemsView == 1) {';
+	if ($sef) {
+		$jsPart1 .= '   document.location 		= jQuery.param.querystring(urlItemsView, currentUrlParams, 2);';
+	} else {
+		$jsPart1 .= '   phRemoveFilter(param, value, isItemsView, urlItemsView, uniqueValue, wait);';
+	}
+	$jsPart1 .= ' } else {'
+		.'   document.location 		= urlItemsView;'	
+		.' }';
 }
 
 $jsPart2 = PhocaCartRenderJs::renderLoaderFullOverlay();
-
 
 $js   = array();
 $js[] = 'function phChangeFilter(param, value, formAction, formType, uniqueValue, wait) {';
@@ -84,7 +116,7 @@ $js[] = '      } else {';
 $js[] = '         phRemoveFilter(param, value, isItemsView, urlItemsView, uniqueValue, wait);';
 $js[] = '      }';
 $js[] = '   } else if (formType == \'category\') {';
-$js[] = '      urlItemsView			= \''.$urlItemsViewWithoutParams.'\';';
+$js[] = ' 		urlItemsView = \''.$urlItemsViewWithoutParams.'\';';
 $js[] = '      ' . $jsPart1 ;
 $js[] = '   } else {';
 $js[] = '      if (formAction.checked) {';
